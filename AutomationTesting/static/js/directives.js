ccb0a1795aba436160c5e00a3e4b5c64a5e33717
MohanT <mohan.tadikimalla@fireeye.com>
2017-01-28 21:34:48 +0530

    fixed basic css styles

--- AutomationTesting/static/js/directives.js
+++ AutomationTesting/static/js/directives.js
@@ -19,7 +19,7 @@ directives.directive('utPanelSet',function(){
 	    scope: {
 	    	config: '='
 	    },
-	    template: '<div class="divTable" ng-transclude></div>',
+	    template: '<div class="ut-panel-container" ng-transclude></div>',
 		controller: function($scope){
 			this.panels = $scope.panels = [];
 			this.addPanel = function(panel){
@@ -37,9 +37,9 @@ directives.directive('utPanelHeading',function(){
 		restrict: 'EA',
 		replace: true,
 		require: '^utPanelSet',
-		template: '<div class="panel">'+
-					'<div class="panel-header clearfix">'+
-				  		'<div class="panel-cell" ng-style="setHeadStyles(col, $index)" ng-repeat="col in config">'+
+		template: '<div class="ut-panel-header">'+
+					'<div class="ut-panel-row clearfix">'+
+				  		'<div class="ut-panel-cell" ng-style="setHeadStyles(col, $index)" ng-repeat="col in config">'+
 				  			'{{col.columnName}}'+
 				  		'</div>'+
 				  	'</div>'+
@@ -62,9 +62,9 @@ directives.directive('utPanel',function(){
 		restrict: 'EA',
 		replace: true,
 		require: '^utPanelSet',
-		template: '<div class="panel">'+
-					'<div class="panel-header clearfix">'+
-						'<div class="panel-cell" ng-style="setPanelCellStyles(col, $index)" ng-repeat="col in config" ng-switch="col.map" ng-click="toggleCollapse()">'+
+		template: '<div class="ut-panel-body">'+
+					'<div class="ut-panel-row clearfix">'+
+						'<div class="ut-panel-cell" ng-style="setPanelCellStyles(col, $index)" ng-repeat="col in config" ng-switch="col.map" ng-click="toggleCollapse()">'+
 							'<div ng-switch-when="id">{{data.id}}</div>'+
 							'<div ng-switch-when="owner">{{data.owner}}</div>'+
 							'<div ng-switch-when="timestarted">{{data.timestarted}}</div>'+
@@ -76,7 +76,7 @@ directives.directive('utPanel',function(){
 							'<ANY ng-switch-default>No Data</ANY>'+
 						'</div>'+
 					'</div>'+
-					'<div class="panel-body panelSlideUp" ng-hide="collapse">'+
+					'<div class="ut-panel-expand-body" ng-hide="collapse">'+
 						'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,'+
 					'</div>'+
 				  '</div>',
